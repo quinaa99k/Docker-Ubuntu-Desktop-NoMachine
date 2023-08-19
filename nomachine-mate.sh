d5f5006fe62962aa812c8de31e6fb758671f0503
quinaa99k <142360831+quinaa99k@users.noreply.github.com>
2023-08-18 23:34:12 -0700

    Update nomachine-mate.sh

--- nomachine-mate.sh
+++ nomachine-mate.sh
@@ -14,13 +14,24 @@ function goto
 
 : ngrok
 clear
-
-./ngrok authtoken 2U5TcRkDqTMQq0f8YfNnajfxaFB_6rfmYNFLX69tVr1ALq6X6 
-
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
 
 clear
-
-./ngrok tcp --region us 4000 &>/dev/null &
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-mate -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:mate
